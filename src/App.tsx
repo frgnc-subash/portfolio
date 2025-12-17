637d7d4fdd2bdc84d6a679e2045a3a2e2731201b
frgnc-subash <axosis.social357@gmail.com>
2025-12-17 19:37:48 +0545

    feat | base folder structure setup & under construction message

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+    <div className="min-h-screen  text-white flex flex-col items-center justify-center p-4">
+      <div className="text-center mb-12">
+        <h1 className="text-4xl md:text-4xl font-bold mb-4">
+          Under Construction
+        </h1>
+        <p className="text-gray-400 text-lg md:text-xl">
+          Hi Visitor! I'm currently building something specific here.
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <p className="text-amber-600 mb-3.5">You can find me here!</p>
+      <div className="flex items-center space-x-4 text-gray-500 font-medium">
+        <a
+          href="https://github.com/frgnc-subash"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-white transition-colors duration-300"
+        >
+          Github
+        </a>
+        <span>|</span>
+        <a
+          href="https://instagram.com/frgnc.subash"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-pink-400 transition-colors duration-300"
+        >
+          Instagram
+        </a>
+        <span>|</span>
+        <a
+          href="https://discord.https://discord.gg/Xjwm9mVvQW"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-indigo-400 transition-colors duration-300"
+        >
+          Discord
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
