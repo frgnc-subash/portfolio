9c75f6fe48b834ec1e96d6264ae11ceb8c4e8f42
frgnc-subash <axosis.social357@gmail.com>
2025-12-30 22:41:44 +0545

    feat | base pages

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,5 +1,11 @@
 const PageNotFound = () => {
-  return <div>Page Not Found</div>;
+  return (
+    <div className="flex h-[80vh] items-center justify-center">
+      <h1 className="text-xl font-medium tracking-widest text-zinc-500 dark:text-zinc-400 uppercase">
+        404 <span className="mx-4 text-zinc-300 dark:text-zinc-700">|</span> Page Not Found
+      </h1>
+    </div>
+  );
 };
 
 export default PageNotFound;
