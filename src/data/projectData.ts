80dca8d42ee11cff39a47a89471b08863e720c10
frgnc-subash <axosis.social357@gmail.com>
2026-02-13 12:57:20 +0545

    feat | update project info

--- src/data/projectData.ts
+++ src/data/projectData.ts
@@ -23,20 +23,19 @@ export const PROJECTS: Project[] = [
   },
   {
     id: "2",
-    title: "Mount-Treks",
-    description:
-      "A commercial e-commerce website promoting & featuring tourism in Nepal",
-    tech: ["React", "Typescript", "MongoDB Atlas", "NodeJS"],
+    title: "Altigo Himalayan Treks",
+    description: "A commercial e-commerce website promoting & featuring tourism in Nepal",
+    tech: ["React", "TypeScript", "NodeJS"],
     link: "https://mounttreks.com",
     github: "https://github.com/frgnc-subash/mount-treks",
     isFeatured: true,
   },
   {
     id: "3",
-    title: "Velvet: Hotel Management",
+    title: "Velvet: Hall Management System",
     description:
       "A Java-based application for managing hotel tasks, bookings, and productivity metrics.",
-    tech: ["Java", "Core", "CMS"],
+    tech: ["Java", "Spring", "Maven"],
     link: "https://github.com/frgnc-subash/velvet",
     github: "https://github.com/frgnc-subash/velvet",
     stars: 1,
@@ -44,10 +43,11 @@ export const PROJECTS: Project[] = [
   {
     id: "4",
     title: "Architectfolio",
-    description: "A minimal and slick portfolio designed for an architecture student.",
+    description: "A minimal and slick portfolio designed for my Architect Brother.",
     tech: ["React", "Typescript", "Minimal"],
     link: "https://sajan-lama.vercel.app",
     github: "https://github.com/frgnc-subash",
     stars: 1,
   },
 ];
+
